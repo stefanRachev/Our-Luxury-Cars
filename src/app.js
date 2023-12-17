9006bd7b8f1a4b185a12253c17871aef59850595
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2023-12-17 15:36:36 +0200

    create homePage,change html element nav

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 import page from "../node_modules/page/page.mjs";
-import { navigation,renderContent } from "./middleware/render.js";
+import { navigation, renderContent } from "./middleware/render.js";
 import { userSession } from "./middleware/userSession.js";
+import { homePage } from "./views/homePage.js";
 import { loginPage } from "./views/loginPage.js";
 import { registerPage } from "./views/registerPage.js";
 import { searchPage } from "./views/searchPage.js";
+
 
 page(userSession);
 page(navigation);
 page(renderContent);
 
+page("/", homePage);
 page("/register", registerPage);
 page("/login", loginPage);
 page("/search", searchPage);
--- src/middleware/render.js
+++ src/middleware/render.js
@@ -1,7 +1,6 @@
-import { render,html } from "../../node_modules/lit-html/lit-html.js";
+import { render, html } from "../../node_modules/lit-html/lit-html.js";
 
-
-const headerNavigation = document.querySelector(".header-navigation");
+const headerNavigation = document.querySelector(".nav-navigation");
 
 const mainElement = document.getElementById("main-element");
 
@@ -10,30 +9,23 @@ const renderModel = (template) => {
 };
 
 const navigationTemplate = (user) => html`
-  <a id="logo" href="/"
-    ><img id="logo-car" src="/images/car-logo.png" alt="img"
-  /></a>
-  <nav>
-    <div>
-      <a href="/cars">Our Cars</a>
-      <a href="/search">Search</a>
-    </div>
-    ${user
-      ? html` <div class="user">
-          <a href="/user/profile">Add Your Car</a>
-          <a href="/logout">Logout</a>
-        </div>`
-      : html`
-    <div class="guest">
-      <a href="/login">Login</a>
-      <a href="/register">Register</a>
-    </div>
-  </nav>`}
-  </nav>
+  <div>
+    <a href="/cars">Our Cars</a>
+    <a href="/search">Search</a>
+  </div>
+  ${user
+    ? html` <div class="user">
+        <a href="/user/profile">Add Your Car</a>
+        <a href="/logout">Logout</a>
+      </div>`
+    : html` <div class="guest">
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </div>`}
 `;
 
 export const navigation = (ctx, next) => {
-  render(navigationTemplate(ctx.user),headerNavigation);
+  render(navigationTemplate(ctx.user), headerNavigation);
   next();
 };
 
