0b317b841cc4236fb4676c2e9eaec6068c959b88
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2023-12-17 21:30:37 +0200

    implement functionality catalogPage

--- src/views/catalogPage.js
+++ src/views/catalogPage.js
@@ -1,42 +1,42 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
 
-const catalogTemplate = () => html`
+const catalogTemplate = (cars) => html`
   <h3 class="heading">Our Cars</h3>
   <section id="dashboard">
-    <!-- Display a div with information about every post (if any)-->
-    <div class="car">
-      <img src="./images/pagani.jpg" alt="example1" />
-      <h3 class="model">Pagani Huayra</h3>
-      <div class="specs">
-        <p class="price">Price: €1,010,310</p>
-        <p class="weight">Weight: 1350 kg</p>
-        <p class="top-speed">Top Speed: 360 kph</p>
-      </div>
-      <a class="details-btn" href="#">More Info</a>
-    </div>
-    <div class="car">
-      <img src="./images/martin.png" alt="example1" />
-      <h3 class="model">Aston Martin One-77</h3>
-      <div class="specs">
-        <p class="price">Price: €2,960,000</p>
-        <p class="weight">Weight: 1500 kg</p>
-        <p class="top-speed">Top Speed: 355 kph</p>
-      </div>
-      <a class="details-btn" href="#">More Info</a>
-    </div>
-    <div class="car">
-      <img src="./images/ferrari.png" alt="example1" />
-      <h3 class="model">Ferrari LaFerrari</h3>
-      <div class="specs">
-        <p class="price">Price: €3,210,000</p>
-        <p class="weight">Weight: 1600 kg</p>
-        <p class="top-speed">Top Speed: 355 kph</p>
-      </div>
-      <a class="details-btn" href="#">More Info</a>
-    </div>
+    ${cars.length < 1
+      ? html`<h3 class="nothing">Nothing to see yet</h3>`
+      : cars.map(
+          (car) => html`
+            <div class="car">
+              <img src="${car.imageUrl}" alt="example1" />
+              <h3 class="model">${car.model}</h3>
+              <div class="specs">
+                <p class="price">Price: ${car.price}</p>
+                <p class="weight">Weight: ${car.weight}</p>
+                <p class="top-speed">Top Speed: ${car.speed}</p>
+              </div>
+              <a class="details-btn" href="/details/${car._id}">More Info</a>
+            </div>
+          `
+        )}
   </section>
 `;
 
-export const catalogPage = (ctx) => {
-  ctx.render(catalogTemplate());
+export const catalogPage = async (ctx) => {
+  const url = "http://localhost:3030/data/cars?sortBy=_createdOn%20desc";
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    const cars = await response.json();
+
+    ctx.render(catalogTemplate(cars));
+  } else {
+    console.error(`HTTP error! Status: ${response.status}`);
+  }
 };
