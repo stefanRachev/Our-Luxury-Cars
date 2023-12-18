e7f88494eafd09249f7d62190cb06551329535ec
stefanRachev <122920341+stefanRachev@users.noreply.github.com>
2023-12-18 13:55:25 +0200

    implement functionality detailsPage

--- src/views/detailsPage.js
+++ src/views/detailsPage.js
@@ -1,37 +1,50 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
 
-const detailsTemplate = () => html`
+const detailsTemplate = (car, owner) => html`
   <section id="details">
     <div id="details-wrapper">
-      <img id="details-img" src="./images/pagani.jpg" alt="example1" />
-      <p id="details-title">Pagani Huayra</p>
+      <img id="details-img" src="${car.imageUrl}" alt="example1" />
+      <p id="details-title">${car.model}</p>
       <div id="info-wrapper">
         <div id="details-description">
-          <p class="price">Price: €1,010,310</p>
-          <p class="weight">Weight: 1350 kg</p>
-          <p class="top-speed">Top Speed: 360 kph</p>
-          <p id="car-description">
-            The Pagani Huayra is a breathtaking hypercar that seamlessly blends
-            art and engineering, representing the epitome of automotive
-            craftsmanship. Its striking exterior features aerodynamic curves and
-            gull-wing doors, showcasing a design that's both futuristic and
-            elegant. Underneath the hood, a meticulously crafted AMG-sourced V12
-            engine delivers an exhilarating performance, propelling the Huayra
-            to astounding speeds, while the opulent interior envelops the driver
-            in a cocoon of luxury, making every journey a symphony of power and
-            refinement.
-          </p>
+          <p class="price">Price: ${car.price}</p>
+          <p class="weight">Weight: ${car.weight}</p>
+          <p class="top-speed">Top Speed: ${car.speed}</p>
+          <p id="car-description">${car.about}</p>
         </div>
         <!--Edit and Delete are only for creator-->
-        <div id="action-buttons">
-          <a href="" id="edit-btn">Edit</a>
-          <a href="" id="delete-btn">Delete</a>
-        </div>
+        ${car._ownerId === owner
+          ? html`
+              <div id="action-buttons">
+                <a href="" id="edit-btn">Edit</a>
+                <a href="" id="delete-btn">Delete</a>
+              </div>
+            `
+          : ""}
       </div>
     </div>
   </section>
 `;
 
-export const detailsPage = (ctx) => {
-  ctx.render(detailsTemplate());
+export const detailsPage = async (ctx) => {
+  const id = ctx.params.id;
+  const owner = ctx.user._id;
+  console.log(owner);
+
+  const url = `http://localhost:3030/data/cars/${id}`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    const car = await response.json();
+    console.log(car);
+    ctx.render(detailsTemplate(car,owner));
+  } else {
+    console.error(`HTTP error! Status: ${response.status}`);
+  }
 };
